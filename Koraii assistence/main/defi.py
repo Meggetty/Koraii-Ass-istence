1b6255f0cd9f723d98034a17b66fca21c546bd0c
Meg <100376847+Meggetty@users.noreply.github.com>
2022-06-28 20:46:28 -0500

    Add files via upload

---
+++ Koraii assistence/main/defi.py
@@ -0,0 +1,35 @@
+from calendar import month
+from datetime import datetime
+import DateTime
+from playsound import playsound
+import time 
+
+#audio execute and text.
+
+def welcoming():
+    print("Hello, is nice to meet you.")
+    playsound('./hello is nice to meet you.mp3')
+
+def leaving():
+    print("I'll miss you. ") 
+    playsound('./goodbye.mp3') 
+
+def suddenlyquit():
+    print("¿did I did something wrong?")
+    playsound('./suddenlyquit.mp3')
+
+def easteregg():
+    print("you are my best friend forever.")
+    playsound('./easteregg.mp3')
+
+
+#date def
+
+
+
+
+
+#main
+def welcoming2():
+    print("¿How can I help you?")
+    
---
+++ Koraii assistence/main/main.py
@@ -0,0 +1,5 @@
+from defi import welcoming, leaving, suddenlyquit, easteregg, welcoming2
+
+welcoming() 
+welcoming2()
+
