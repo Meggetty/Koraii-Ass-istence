6a663ea2d673100d83431ef9cb4397a08ee27616
Meggetty <johndoe1234567q@gmail.com>
2022-07-11 19:18:49 -0500

    Audio commands

--- Koraii assistence/main/main.py
+++
@@ -1,5 +0,0 @@
-from defi import welcoming, leaving, suddenlyquit, easteregg, welcoming2
-
-welcoming() 
-welcoming2()
-
--- main/defi.py
+++ main/defi.py
@@ -1,10 +1,8 @@
-from calendar import month
-from datetime import datetime
-import DateTime
-from playsound import playsound
-import time 
 
-#audio execute and text.
+import playsound
+from playsound import playsound
+
+#comandos de ejecucion de audio
 
 def welcoming():
     print("Hello, is nice to meet you.")
@@ -13,6 +11,7 @@ def welcoming():
 def leaving():
     print("I'll miss you. ") 
     playsound('./goodbye.mp3') 
+    
 
 def suddenlyquit():
     print("¿did I did something wrong?")
@@ -22,14 +21,7 @@ def easteregg():
     print("you are my best friend forever.")
     playsound('./easteregg.mp3')
 
-
-#date def
-
-
-
-
-
-#main
+#fuciones de interaccion que requieren del frontend
 def welcoming2():
     print("¿How can I help you?")
     
